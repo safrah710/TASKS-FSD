7679e286509dc01575332706342829fb0bcf718f
safrah710 <safeerahuman2004@gmail.com>
2024-04-04 18:09:02 +0530

    success

---
+++ task2.js
@@ -0,0 +1,19 @@
+/*
+ FSD mainboot camp 
+Assignment 2
+1.
+**Document Object:**
+1. Represents the HTML document and provides methods and properties to manipulate its content.
+2. Accessed using `document` in the browser environment, offering an interface to interact with the document's structure, elements, and styles.
+3. Primarily deals with the content within the HTML, such as elements, text, and attributes.
+4. It includes methods like `getElementById` and `querySelector` to access and manipulate specific elements.
+5. The `document` object is specific to the loaded HTML document and doesn't encompass the entire browser window.
+
+**Window Object:**
+1. Represents the browser window and serves as the global object in the browser's JavaScript environment.
+2. Accessed using `window`, providing properties and methods for managing the browser window, navigation, and global scope.
+3. Deals with broader aspects, including navigation (`window.location`), handling events, and managing browser history.
+4. It includes methods like `setTimeout` and `setInterval` for executing code asynchronously and repeatedly.
+5. The `window` object encapsulates the entire browser environment and serves as the global scope for all JavaScript running in that context.
+
+*/
